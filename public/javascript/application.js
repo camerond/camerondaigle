b7a0a4e5375778f3d032785031b7eb8d94826b9a
Cameron Daigle <cameron@hashrocket.com>
2011-03-01 23:43:13 -0500

    Remove commented js code

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,8 @@
 $(function() {
+
   $("#article_show img").each(function() {
     var $img = $(this);
     $img.parent('p').addClass('image').append($img.attr('alt'));
   });
-  
-  // $(window).resize(function() {
-  //   var $f = $("footer");
-  //   var wh = $(this).height();
-  //   console.log(wh + ' ' + $("#content").outerHeight())
-  //   var h = wh > $("#content").outerHeight() ?
-  //     wh - $f.outerHeight() - $('header').outerHeight() : 'auto';
-  //   console.log(h)
-  //   $("#wrapper").height(h);
-  // 
-  // }).trigger("resize");
 
 });
