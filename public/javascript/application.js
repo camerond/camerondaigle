a6c522fa8fee25a08fa352822ffe88e5e1894d4f
Cameron Daigle <camerondaigle@gmail.com>
2013-02-26 22:55:21 -0500

    Update images to be responsive and in figure elements

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,8 +1,11 @@
 $(function() {
 
   $("#article_show img").each(function() {
-    var $img = $(this);
-    $img.parent('p').addClass('image').append($img.attr('alt'));
+    var $img = $(this),
+        $p = $img.closest('p'),
+        $figure = $("<figure />").insertBefore($p).html($p.html());
+    $("<figcaption />").appendTo($figure).text($img.attr('alt'));
+    $p.remove();
   });
 
 });
