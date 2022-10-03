9e6b50cb733cf152423e829bc63084356053a7fb
Cam Daigle <camerondaigle@gmail.com>
2022-10-03 16:45:02 -0500

    Add passthru, remove www from cname

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
 
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
